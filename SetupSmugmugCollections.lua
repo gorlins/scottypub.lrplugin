93b830b00b6a6d8502d81fa8f190b993313d723c
Scott Gorlin <scottgorlin@gmail.com>
2020-10-22 15:53:14 -0400

    Revert "Update smugmug for recent times"
    This reverts commit 4886e5f755c5b90000f830c01971632766ef593d.

--- SetupSmugmugCollections.lua
+++ SetupSmugmugCollections.lua
@@ -70,7 +70,7 @@ function MyHWExportItem.setup_smugmug_collections()
     --LrDialogs.message("ExportMenuItem Selected", to_string(archive:getCollectionSetInfoSummary()['collectionSettings']), "helloworld info")
     
     local months = {"01", "02", "03", '04', '05', '06', '07', '08', '09', '10', '11', '12'}
-    local years = {"2019", "2020"}
+    local years = {"2000", "2001", "2002", '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015'}
     
     for yi, year in pairs(years) do
       
